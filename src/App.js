6c263d89b99de8b844a10942cc422cfbc6a03b50
georgeSamir3 <georgesamirabd@gmail.com>
2023-04-17 20:55:53 +0200

    first push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Switch, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Register from "./pages/Register";
+import PrivateRoute from "./components/PrivateRoute";
+import Login from "./pages/Login";
+import AuthProvider from "./context/auth";
+import React from "react";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          {/* <React.Fragment>
+            <PrivateRoute path="/" element={<Home></Home>}></PrivateRoute>
+          </React.Fragment> */}
+          <Route exact path="/register" element={<Register></Register>} />
+          <Route exact path="/login" element={<Login></Login>} />
+          {/* <PrivateRoute exact path="/profile" component={Profile} /> */}
+          <Route path="/profile" element={<Profile></Profile>}></Route>
+          <Route exact path="/" element={<Home></Home>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
